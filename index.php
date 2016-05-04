d8525761928dccb4037938c38fc8a4f21a3e28d6
volcimaster <volcimaster@gmail.com>
2016-05-04 15:51:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -126,7 +126,7 @@
 <div id="footer">
 <address>Copyright &copy; 2016-<?php echo date("Y")." ";?>
 by <a href="//antipaucity.com">Warren Myers</a>. 
-Somce rights reserved - <a href="https://creativecommons.org/licenses/by/4.0">CC BY 4.0</a>. 
+Some rights reserved - <a href="https://creativecommons.org/licenses/by/4.0">CC BY 4.0</a>. 
 Loaded <?php echo date("jS M Y H:i");?> UTC.
 </address>
 </div>
